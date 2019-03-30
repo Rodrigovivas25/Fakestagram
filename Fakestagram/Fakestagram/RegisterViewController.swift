0d144a7973d327e3d9b6f7b910f0ac6ab770f01d
Rodrigo <rodrigo_diplomado@macbook-7.local>
2019-03-30 13:50:43 -0600

    Se creó la interfaz del login

---
+++ Fakestagram/Fakestagram/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  Fakestagram
+//
+//  Created by Rodrigo Vivas Maldonado D3 on 3/30/19.
+//  Copyright © 2019 RodrigoV. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backToMain(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
